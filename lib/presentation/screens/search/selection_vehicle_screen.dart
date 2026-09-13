bab72149bc1f7254dc425e0d697a966db0837881
Altureny <altureny17@gmail.com>
2026-09-13 18:07:45 +0300

    Fix import path for send_ride_request_screen.dart

--- lib/presentation/screens/search/selection_vehicle_screen.dart
+++ lib/presentation/screens/search/selection_vehicle_screen.dart
@@ -7,7 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ride_on/core/utils/translate.dart';
-import 'package:ride_on/presentation/screens/Search/send_ride_request_screen.dart';
+import 'package:ride_on/presentation/screens/search/send_ride_request_screen.dart';
 import '../../../core/services/data_store.dart';
 import '../../../core/utils/common_widget.dart';
 import '../../../core/utils/theme/project_color.dart';
