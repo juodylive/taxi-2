2c47ae96dce6273c312878de9ecaf872bde7df7d
Altureny <altureny17@gmail.com>
2026-09-20 17:43:42 +0300

    Switch to Nominatim API for location services
    Replaced Google Maps API with Nominatim for geocoding and reverse geocoding. Updated related methods to use new API endpoints and headers.

--- lib/presentation/cubits/location/user_current_location_cubit.dart
+++ lib/presentation/cubits/location/user_current_location_cubit.dart
@@ -5,11 +5,14 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
  import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:latlong2/latlong.dart';
 // ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 
-import '../../../core/services/config.dart';
+const String nominatimBaseUrl = 'https://nominatim.openstreetmap.org';
+const Map<String, String> nominatimHeaders = {
+  'User-Agent': 'ZearahRiderApp/1.0 (contact@zearah.dpdns.org)'
+};
 
 abstract class LocationState extends Equatable {
   @override
@@ -42,7 +45,7 @@ class LocationFailure extends LocationState {
 class LocationUserCubit extends Cubit<LocationState> {
   LocationUserCubit() : super(LocationInitial());
   late StreamSubscription<Position> positionStreamSubscription;
-  var markers = <Marker>{};
+  var markers = <dynamic>{};
 
   void startLiveLocationTracking({bool? ischeckedLoading}) async {
     try {
@@ -144,17 +147,16 @@ class UpdateCurrentAddressCubit extends Cubit<UpdateCurrentAddressState> {
     try {
       emit(UpdateCurrentAddressLoading());
 
-      const String googleApiKey = Config.googleKey;
       if (latitude != null && longitude != null) {
         String url =
-            "https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$googleApiKey";
+            "$nominatimBaseUrl/reverse?format=json&lat=$latitude&lon=$longitude";
 
-        final response = await http.get(Uri.parse(url));
+        final response = await http.get(Uri.parse(url), headers: nominatimHeaders);
 
         if (response.statusCode == 200) {
           var data = json.decode(response.body);
-          if (data["status"] == "OK") {
-            String address = data["results"][0]["formatted_address"];
+          if (data["display_name"] != null) {
+            String address = data["display_name"];
             emit(UpdateCurrentAddresSuccess(
                 currentAddress: address, lat: latitude, lng: longitude));
           } else {
@@ -200,17 +202,16 @@ class UpdateSearchMapAddressCubit extends Cubit<UpdateSearchMapAddressState> {
   Future<void> getAddressFromLatLng(
       {double? latitude, double? longitude}) async {
     try {
-      String googleApiKey = Config.googleKey;
       if (latitude != null && longitude != null) {
         String url =
-            "https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$googleApiKey";
+            "$nominatimBaseUrl/reverse?format=json&lat=$latitude&lon=$longitude";
 
-        final response = await http.get(Uri.parse(url));
+        final response = await http.get(Uri.parse(url), headers: nominatimHeaders);
 
         if (response.statusCode == 200) {
           var data = json.decode(response.body);
-          if (data["status"] == "OK") {
-            String address = data["results"][0]["formatted_address"];
+          if (data["display_name"] != null) {
+            String address = data["display_name"];
             emit(UpdateSearchMapAddresSuccess(
                 currentAddress: address, lat: latitude, lng: longitude));
           } else {
@@ -256,22 +257,19 @@ class GetSuggestionAddressCubit extends Cubit<GetSuggestionAddressState> {
 
 
 
-    const apiKey = Config.googleKey;
-
     final url = Uri.parse(
-        "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${Uri.encodeQueryComponent(query)}&key=$apiKey");
+        "$nominatimBaseUrl/search?format=json&addressdetails=0&limit=5&q=${Uri.encodeQueryComponent(query)}");
 
     try {
-      final response = await http.get(url);
+      final response = await http.get(url, headers: nominatimHeaders);
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> jsonResponse = json.decode(response.body);
+        final List jsonResponse = json.decode(response.body);
 
-        if (jsonResponse.containsKey("predictions") &&
-            jsonResponse["predictions"].isNotEmpty) {
-          final List data = jsonResponse["predictions"];
-          List<String> suggestions =
-              data.map<String>((place) => place["description"]).toList();
+        if (jsonResponse.isNotEmpty) {
+          List<String> suggestions = jsonResponse
+              .map<String>((place) => place["display_name"].toString())
+              .toList();
 
           emit(GetSuggestionAddressSuccess(suggestions: suggestions));
         } else {
@@ -322,25 +320,22 @@ class GetCordinatesCubit extends Cubit<GetCordinatesState> {
   Future<void> getCoordinates(
       {required String address, bool? checkStatus}) async {
     try {
-      const String googleApiKey = Config.googleKey;
       final url = Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?address=${Uri.encodeComponent(address)}&key=$googleApiKey',
+        '$nominatimBaseUrl/search?format=json&limit=1&q=${Uri.encodeComponent(address)}',
       );
 
-      final response = await http.get(url);
+      final response = await http.get(url, headers: nominatimHeaders);
 
       if (response.statusCode == 200) {
-        final data = json.decode(response.body);
+        final List data = json.decode(response.body);
 
-        if (data['status'] == 'OK') {
-          final location = data['results'][0]['geometry']['location'];
-          final latitude = location['lat'].toString();
-          final longitude = location['lng'].toString();
-
+        if (data.isNotEmpty) {
+          final latitude = data[0]['lat'].toString();
+          final longitude = data[0]['lon'].toString();
 
           emit(GetCordinatesSuccess(lattiude: latitude, longitude: longitude));
         } else {
-          emit(GetCordinatesFailure(error: data['status']));
+          emit(GetCordinatesFailure(error: "No results"));
         }
       } else {
         emit(GetCordinatesFailure(error: "HTTP error: ${response.statusCode}"));
